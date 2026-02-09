417664fa42827e7890b65cbf083c931ba367d0e8
Ravi Bhatt <ravi2018bhatt@gmail.com>
2026-02-09 11:57:19 +0545

    Implement weather fetching in WeatherPage with loading and error handling

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,17 @@ import 'package:weather_app/app.dart';
 import 'package:weather_app/src/features/weather/data/weather_api_service.dart';
 import 'package:weather_app/src/features/weather/data/models/weather_model.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
-
-void main() async {
-  Weather? weather = await WeatherApiService.fetchWeather('Kathmandu');
-
-  if (weather != null) {
-    print("City: ${weather.cityName}");
-    print("Temperature: ${weather.temp}°C");
-  } else {
-    print("Could not fetch weather");
-  }
+void main() {
+  runApp(const MyApp());
 }
+
+// void main() async {
+//   Weather? weather = await WeatherApiService.fetchWeather('Kathmandu');
+
+//   if (weather != null) {
+//     print("City: ${weather.cityName}");
+//     print("Temperature: ${weather.temp}°C");
+//   } else {
+//     print("Could not fetch weather");
+//   }
+// }
--- lib/src/features/weather/presentation/view/weather_page.dart
+++ lib/src/features/weather/presentation/view/weather_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/src/features/weather/data/models/weather_model.dart';
+import 'package:weather_app/src/features/weather/data/weather_api_service.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -8,8 +10,53 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  Weather? _weather;
+  bool _isLoading = true;
+  String? _error;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchWeather();
+  }
+
+  Future<void> _fetchWeather() async {
+    setState(() {
+      _isLoading = true;
+      _error = null;
+    });
+
+    final result = await WeatherApiService.fetchWeather("Kathmandu");
+
+    setState(() {
+      if (result != null) {
+        _weather = result;
+      } else {
+        _error = "Failed to load weather";
+      }
+      _isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text("Weather App"), centerTitle: true),
+      body: Center(
+        child: _isLoading
+            ? const CircularProgressIndicator()
+            : _error != null
+            ? Text(_error!)
+            : Column(
+                spacing: 100,
+                mainAxisAlignment: .center,
+                children: [
+                  Text("City: ${_weather!.cityName}", style: TextStyle(fontSize: 25)),
+                  Text("${_weather!.temp}°C", style: TextStyle(fontSize: 45)),
+                  SizedBox(height: 10),
+                ],
+              ),
+      ),
+    );
   }
 }
